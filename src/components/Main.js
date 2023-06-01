f343c09cd02cac1f32a455374b5bf96a506d9f89
Raneemoqaily7 <raneemoqaily7@gmail.com>
2023-06-01 00:39:46 +0000

    configure map

---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
+
+function MapComponent() {
+  const [mapHtml, setMapHtml] = useState('');
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/index/')
+      .then((res)=>{
+console.log(res.data.map ,"resss")
+setMapHtml(res.data.map)
+      })
+      
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+  return(
+    <>
+    <div>{mapHtml}</div>
+  
+    </>
+  );
+}
+
+export default MapComponent;
